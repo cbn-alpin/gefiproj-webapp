b5dbfa24c8fb632a3e228ac88e86357e514757bb
manu <tempo05@laposte.net>
2020-10-09 20:00:29 +0200

    Gestion du Token de connexion

---
+++ src/app/services/user.ts
@@ -0,0 +1,14 @@
+/**
+ * Réprésente un login/password.
+ */
+export interface User {
+    /**
+     * Nom de connexion.
+     */
+    login: string,
+
+    /**
+     * Mot de passe de connexion.
+     */
+    password: string
+}
