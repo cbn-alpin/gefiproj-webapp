656bd88a61d18eebb22d8fc3badfd2a5eab5ad98
alexandre_perr1 <labtecn0x7@gmail.com>
2020-11-05 12:00:59 +0100

    feat (projet): create components

---
+++ src/app/components/projet/components/projet-details/projet-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projet-details',
+  templateUrl: './projet-details.component.html',
+  styleUrls: ['./projet-details.component.scss']
+})
+export class ProjetDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
