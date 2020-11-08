e0312629078d05c260cc0b1fcf3f949fee8356d3
manu <tempo05@laposte.net>
2020-11-08 22:04:38 +0100

    Home : récupération du tableau générique

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
+import { GenericTableOptions } from './../../shared/components/generic-table/models/generic-table-options';
 import { Projet } from './../../models/projet';
 import { Component, OnInit } from '@angular/core';
-import { ProjetsService } from './projets.service';
+import { ProjetsService } from '../../services/projets.service';
+import { GenericTableCellType } from 'src/app/shared/components/generic-table/globals/generic-table-cell-types';
 
 /**
  * Affiche les projets.
@@ -17,11 +19,37 @@ export class HomeComponent implements OnInit {
   projets: Projet[] = [];
 
   /**
+   * Titre du tableau générique
+   */
+  readonly title = 'Projets';
+  options: GenericTableOptions<Projet> = {
+    dataSource: [],
+    defaultEntity: {
+      id: -1,
+      code: -1,
+      nom: '<nouveau projet>',
+      responsable: '',
+      statut: false
+    },
+    entityTypes: [
+      { name: 'id', type: GenericTableCellType.NUMBER },
+      { name: 'code', type: GenericTableCellType.NUMBER },
+      { name: 'nom', type: GenericTableCellType.TEXT },
+      { name: 'responsable', type: GenericTableCellType.TEXT },
+      { name: 'statut', type: GenericTableCellType.BOOLEAN }
+    ],
+    entityPlaceHolders: [],
+    entitySelectBoxOptions: []
+  };
+
+  /**
    * Affiche les projets.
    * @param projetsSrv : permet de dialoguer avec le serveur d'API pour les entités Projet.
    */
   constructor(
-    private projetsSrv: ProjetsService) { }
+    private projetsSrv: ProjetsService) {
+
+  }
 
   /**
    * Initialise le composant.
@@ -40,9 +68,24 @@ export class HomeComponent implements OnInit {
   async loadProjects(): Promise<void> {
     try {
       this.projets = await this.projetsSrv.get();
+      this.options = Object.assign({}, this.options, {
+        dataSource: this.projets
+      });
     } catch (error) {
       console.error(error);
       return Promise.reject(error);
     }
+  }
+
+  onEdit(a) {
+
+  }
+
+  onCreate(a) {
+
+  }
+
+  onDelete(a) {
+
   }
 }
