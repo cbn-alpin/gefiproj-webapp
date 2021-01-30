a70ee346909bbd4b3ad71adfbac0fe1d6fe19eff
Goultarminator <labtecn0x7@gmail.com>
2021-01-30 12:17:09 +0100

    Correction test

--- src/app/services/recettes.service.spec.ts
+++ src/app/services/recettes.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RecettesService } from './recettes.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('RecettesService', () => {
   let service: RecettesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(RecettesService);
   });
 
