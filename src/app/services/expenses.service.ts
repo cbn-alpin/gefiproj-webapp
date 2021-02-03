21a9b079f35d57e52a2e6ce715194c23e2690b6e
Goultarminator <labtecn0x7@gmail.com>
2021-02-03 14:25:31 +0100

    Branchement aux appels API

--- src/app/services/expenses.service.ts
+++ src/app/services/expenses.service.ts
@@ -8,7 +8,7 @@ import { SpinnerService } from './spinner.service';
  * Gère les requêtes relatives aux dépenses.
  */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExpensesService {
   /**
@@ -22,38 +22,18 @@ export class ExpensesService {
   private readonly crudSrv: CrudService<Depense>;
 
   /**
-   * TODO : bouchonnage !
-   */
-  private get expenses(): Depense[] {
-    return JSON.parse(localStorage.getItem('debugExpensesService') || '[]') as Depense[];
-  }
-
-  /**
-   * TODO : bouchonnage !
-   */
-  private set expenses(expenses: Depense[]) {
-    localStorage.setItem('debugExpensesService', JSON.stringify(expenses || []));
-  }
-
-  /**
    * Effectue les appels au serveur d'API pour les dépenses.
    * @param http : permet d'effectuer les appels au serveur d'API.
    * @param spinnerSrv : gère le spinner/sablier.
    */
-  constructor(
-    http: HttpClient,
-    spinnerSrv: SpinnerService) {
-      this.crudSrv = new CrudService<Depense>(
-        http,
-        spinnerSrv,
-        this.endPoint);
-    }
+  constructor(http: HttpClient, spinnerSrv: SpinnerService) {
+    this.crudSrv = new CrudService<Depense>(http, spinnerSrv, this.endPoint);
+  }
 
   /**
    * Retourne les dépenses depuis le serveur.
    */
   public async getAll(): Promise<Depense[]> {
-    return this.expenses.slice();
     return this.crudSrv.getAll();
   }
 
@@ -62,7 +42,6 @@ export class ExpensesService {
    * @param id : identifiant de la dépense demandée.
    */
   public async get(id: number): Promise<Depense> {
-    return this.expenses.find(e => e.id_d === id) || null;
     return this.crudSrv.get(id);
   }
 
@@ -71,16 +50,7 @@ export class ExpensesService {
    * @param expense : dépense modifiée.
    */
   public async modify(expense: Depense): Promise<Depense> {
-      const expenses = this.expenses;
-      const index = expenses.findIndex(e => e.id_d === expense.id_d);
-      if (index >= 0) {
-        expenses[index] = expense;
-        this.expenses = expenses;
-      }
-      return expense;
-      return this.crudSrv.modify(
-        expense,
-        expense?.id_d);
+    return this.crudSrv.modify(expense, expense?.id_d);
   }
 
   /**
@@ -88,13 +58,7 @@ export class ExpensesService {
    * @param expense : dépense à créer.
    */
   public async add(expense: Depense): Promise<Depense> {
-      const expenses = this.expenses;
-      expense.id_d = expenses.map(e => e.id_d).reduce((p, c) => Math.max(p, c), 0) + 1;
-      expenses.push(expense);
-      this.expenses = expenses;
-      return expense;
-      return await this.crudSrv
-        .add(expense);
+    return await this.crudSrv.add(expense);
   }
 
   /**
@@ -102,13 +66,6 @@ export class ExpensesService {
    * @param expense : dépense à supprimer.
    */
   public async delete(expense: Depense): Promise<void> {
-    const expenses = this.expenses;
-    const index = expenses.findIndex(e => e.id_d === expense.id_d);
-    if (index >= 0) {
-      expenses.splice(index, 1);
-      this.expenses = expenses;
-    }
-    return;
     const id = expense?.id_d || 0;
     return this.crudSrv.delete(id);
   }
