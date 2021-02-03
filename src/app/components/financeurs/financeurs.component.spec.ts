d222041238fdd413ca9f2ab39d925930fa6574a5
manu <tempo05@laposte.net>
2021-02-03 10:55:32 +0100

    Fixe des UT

--- src/app/components/financeurs/financeurs.component.spec.ts
+++ src/app/components/financeurs/financeurs.component.spec.ts
@@ -1,7 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConnexionComponent } from '../connexion/connexion.component';
+import { HomeComponent } from '../home/home.component';
 import { FinanceursComponent } from './financeurs.component';
+
 
 describe('FinanceursComponent', () => {
   let component: FinanceursComponent;
@@ -10,7 +15,13 @@ describe('FinanceursComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(
+          [{path: 'connexion', component: ConnexionComponent},
+          {path: 'home', component: HomeComponent}]
+        ),
+        MatDialogModule,
+        MatSnackBarModule
       ],
       declarations: [FinanceursComponent]
     })
--- src/app/services/expenses.service.spec.ts
+++ src/app/services/expenses.service.spec.ts
@@ -1,11 +1,22 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConnexionComponent } from '../components/connexion/connexion.component';
+import { HomeComponent } from '../components/home/home.component';
 import { ExpensesService } from './expenses.service';
 
 describe('Service: Expenses', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(
+          [{path: 'connexion', component: ConnexionComponent},
+          {path: 'home', component: HomeComponent}]
+        )
+      ],
       providers: [ExpensesService]
     });
   });
