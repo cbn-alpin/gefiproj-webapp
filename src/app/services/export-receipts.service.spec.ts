cf4eaf61b487accf0a7239b24b3190efb9123237
manu <tempo05@laposte.net>
2021-02-03 16:37:03 +0100

    Fixe un UT

--- src/app/services/export-receipts.service.spec.ts
+++ src/app/services/export-receipts.service.spec.ts
@@ -1,11 +1,22 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConnexionComponent } from '../components/connexion/connexion.component';
+import { HomeComponent } from '../components/home/home.component';
 import { ExportReceiptsService } from './export-receipts.service';
 
 describe('Service: ExportReceipts', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes(
+          [{path: 'connexion', component: ConnexionComponent},
+          {path: 'home', component: HomeComponent}]
+        )
+      ],
       providers: [ExportReceiptsService]
     });
   });
