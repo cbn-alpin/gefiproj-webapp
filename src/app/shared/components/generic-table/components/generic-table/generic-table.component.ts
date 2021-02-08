83a4bc42eb62912dff0bcf6978ec78a25edfdb30
Goultarminator <labtecn0x7@gmail.com>
2021-02-08 09:38:31 +0100

    Re activé les actions du tableau si pas erreurs back

--- src/app/shared/components/generic-table/components/generic-table/generic-table.component.ts
+++ src/app/shared/components/generic-table/components/generic-table/generic-table.component.ts
@@ -280,7 +280,6 @@ export class GenericTableComponent<T>
 
   public edit(event, entity: GenericTableEntity<T>): void {
     event.stopPropagation();
-    this.setDisableActionsValue(false);
     this.genericTableAction = GenericTableAction.EDIT;
     this.showMandatoryIcon = true;
     const genericTableEntityEvent: GenericTableEntityEvent<T> = {
@@ -327,7 +326,6 @@ export class GenericTableComponent<T>
 
   public create(event, entity: GenericTableEntity<T>): void {
     event.stopPropagation();
-    this.setDisableActionsValue(false);
     this.genericTableAction = GenericTableAction.NEW;
     this.showMandatoryIcon = true;
     const genericTableEntityEvent: GenericTableEntityEvent<T> = {
@@ -453,6 +451,7 @@ export class GenericTableComponent<T>
       ) {
         this.setReadEntityState(entity);
       }
+      this.setDisableActionsValue(false);
       this.showMandatoryIcon = false;
       this.endAction.emit();
     }
